81f56154850a12b5d9015eb8d2ed5931a9d97f58
Albin <albin-w@hotmail.com>
2018-09-21 21:20:37 +0200

    Added taskscheduler, wifi and some other goodies

--- src/Common.h
+++ src/Common.h
@@ -17,7 +17,11 @@ extern "C" {
 #include <Arduino.h>
 #include <EEPROM.h>
 #include <OneButton.h>
+#include <TaskScheduler.h>
+#include <ESP8266WiFi.h>
 
+/*  Local libraries  */
+#include "WiFi.h"
 #include "DebugHelpers.h"
 
 //--------------------------------- Settings ----------------------------------/
@@ -26,31 +30,51 @@ extern "C" {
 
 /*  FastLED Settings  */
 #define DATA_PIN            D6
-//#define CLK_PIN             D5          // Only needed for APA102
+//#define CLK_PIN           D5            // Only needed for APA102
 #define LED_TYPE            WS2812B       // WS2812B or APA102
 #define COLOR_ORDER         GRB           // GRB        BGR
-#define NUM_LEDS            25
+#define NUM_LEDS            30
 
 #define VOLTAGE             5
 #define MILLI_AMPS          3000    // IMPORTANT: set the max milli-Amps of your power supply (4A = 4000mA)
 #define FRAMES_PER_SECOND   120     // here you can control the speed. With the Access Point / Web Server the animations run a bit slower.
 
 CRGB leds[NUM_LEDS];
-#include "FastLEDVariables.h"
-#include "FastLEDMethods.h"
 
 /*  Buttons   */
-#define   BTN_ONE_PIN                   D3      // Flash pin
+#define   BTN_ONE_PIN                   D3  // Change mode - Flash pin
+#define   BTN_TWO_PIN                   D7  // Plus 
+#define   BTN_THREE_PIN                 D2  // Minus
 OneButton btnOne(BTN_ONE_PIN, true);
+OneButton btnTwo(BTN_TWO_PIN, true);
+OneButton btnThree(BTN_THREE_PIN, true);
 
-
+/*  Task Manager  */
+Scheduler taskManager;
 
 //-------------------------------- Variables ---------------------------------//
 /*  Button controls  */
 enum ControlMode {
-    First
+  AdjustBrightness,
+  AdjustPattern,
+  AdjustSpeed,
+  Settings
 };
-ControlMode currentBtnMode = First;
+#define NUM_MODES 3 // loopable control modes
+ControlMode currentBtnMode = AdjustBrightness;
 
-#include "Button.h" // Button functions
+/*  Define adjusting speeds  */
+#define ADJUST_INTERVAL                 200 //ms
+#define PRESS_ADJUST_SPEED              20  
+#define LONG_PRESS_ADJUST_SPEED         5
+#define LONG_PRESS_ADJUST_BRIGHTNESS    5
+// brightness adjusting steps are set in FastLEDVariables.h
+
+/*  More local libraries  */
+#include "OTA.h"
+#include "FastLEDVariables.h"   // FastLed variables
+#include "FastLEDMethods.h"     // FastLed functions
+#include "IndicationLeds.h"     // Indication leds functions
+#include "Settings.h"
+#include "Button.h"             // Button functions
 #endif
