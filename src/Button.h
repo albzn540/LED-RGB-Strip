c1966face3cc44657f60d823b08ec81e300d8358
Albin <albin-w@hotmail.com>
2018-09-21 21:20:19 +0200

    Added button related methods

--- src/Button.h
+++ src/Button.h
@@ -3,14 +3,57 @@
 
 #include "Common.h"
 
+void nextMode() {
+  int8_t tmp = currentBtnMode + 1;
+  // Change button mode and wrap around
+  currentBtnMode = (ControlMode) ((tmp >= NUM_MODES) ? 0 : tmp);
+}
+
+void setupButtons() {
+  btnOne.attachClick(nextMode);
+  btnOne.attachLongPressStart(startSettingsMode);
+}
+
 void handleButtons() {
 
   // Update buttons
   btnOne.tick();
+  btnTwo.tick();
+  btnThree.tick();
 
   //Serial.printf("CurrentBtn mode: %d\n ", currentBtnMode);
-  static ControlMode lastMode = None;
+  static ControlMode lastMode = currentBtnMode;
 
+  if(currentBtnMode == Settings) {
+    settingsMode();
+  }
+
+  if(currentBtnMode != lastMode) { // Only run switch statement once per press
+    switch(currentBtnMode) {
+      case AdjustBrightness:
+        Traceln("Adjust brightness mode");
+        btnTwo.attachClick(adjustBrightnessUpCallback);
+        btnThree.attachClick(adjustBrightnessDownCallback);
+        btnTwo.attachDuringLongPress(increaseBrigthnessCallback);
+        btnThree.attachDuringLongPress(decreaseBrigthnessCallback);
+        break;
+
+      case AdjustPattern:
+        Traceln("Adjust pattern mode");
+        btnTwo.attachClick(adjustPatternUpCallback);
+        btnThree.attachClick(adjustPatternDownCallback);
+        break;
+
+      case AdjustSpeed:
+        Traceln("Adjust speed mode");
+        btnTwo.attachClick(adjustSpeedUpCallback);
+        btnThree.attachClick(adjustSpeedDownCallback);
+        btnTwo.attachDuringLongPress(increaseSpeed);
+        btnThree.attachDuringLongPress(decreaseSpeed);
+        break;
+    }
+  }
+  lastMode = currentBtnMode;
 }
 
 #endif
